5254484271aa35fc4cfb4a2c91efa5db300db2ac
safasuli01 <safa.abduallah2001@hotmail.com>
2024-10-09 04:52:07 +0300

    update project models

--- backend/project/models.py
+++ backend/project/models.py
@@ -12,7 +12,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     industry = models.CharField(max_length=100, blank=True, null=True)
-    # author = models.ForeignKey(Individual, on_delete=models.CASCADE)
+    author = models.ForeignKey(Individual, on_delete=models.CASCADE)
     post_status = models.CharField(max_length=100, choices=STATUS, default="Active",blank=True, null=True)
     # created_at = models.DateTimeField(auto_now_add=True)
     budget = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
