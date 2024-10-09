b2ad2606603fb899044095bbbf1e635bec629cb5
safasuli01 <safa.abduallah2001@hotmail.com>
2024-10-09 04:24:44 +0300

    auth urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,3 +1,11 @@
 # from django.urls import path, include
 # from .views import *
 #
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('logout/', UserLogoutView.as_view(), name='user-logout'),
+    path('detail/', UserDetailView.as_view(), name='user-detail'),
+]
