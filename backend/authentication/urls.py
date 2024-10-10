d071420f5dc74b11bde7d206a5c758908978e14c
safasuli01 <safa.abduallah2001@hotmail.com>
2024-10-10 04:54:16 +0300

    add csrf path

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
     path('detail/', UserDetailView.as_view(), name='user-detail'),
+    path('csrf/', get_csrf_token, name='get-csrf-token'),    
 ]
